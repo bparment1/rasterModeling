46ce4cfe234aafd7cef0863a9336d43704469400
Benoit Parmentier <benoit.parmentier@gmail.com>
2017-05-23 08:51:49 -0400

    predicting with SVM regressor

--- temperature_prediction_generate_raster.py
+++ temperature_prediction_generate_raster.py
@@ -367,6 +367,20 @@ rf = RandomForestRegressor(n_estimators = 1000, random_state = random_seed)
 rf.fit(X_train,y_train);
 y_pred_train_rf = rf.predict(X_train);
 y_pred_train_rf.dtype #float32, need to convert to match data type of input raster!!!!
+
+
+from sklearn import svm
+X = [[0, 0], [2, 2]]
+y = [0.5, 2.5]
+clf = svm.SVR()
+clf.fit(X, y) 
+SVR(C=1.0, cache_size=200, coef0=0.0, degree=3, epsilon=0.1,
+    gamma='auto_deprecated', kernel='rbf', max_iter=-1, shrinking=True,
+    tol=0.001, verbose=False)
+clf.predict([[1, 1]])
+array([1.5])
+
+
 #### Model evaluation
 
 #https://scikit-learn.org/stable/modules/classes.html#sklearn-metrics-metrics
@@ -459,7 +473,7 @@ out_filename = "results_linear_regression2.tif"
 test= pdb.runcall(rasterPredict,regr,rast_in,dtype_val,out_filename,out_dir)
 l_regr = rasterPredict(regr,rast_in,dtype_val,out_filename,out_dir)
 r_regr = rasterio.open(l_regr)
-plot.show(r_regr,clim=(0,15))
+plot.show(r_regr,clim=(0,15)) 
 plot.show(r_regr)
 
 array = r_regr.read()
